3bcec02a2e3c5e8f65a104aac9174010645b57ef
AE <aryan.eftekhari@gmail.com>
2015-03-03 01:58:49 +0100

    update

--- install.sh
+++ install.sh
@@ -3,19 +3,22 @@ echo "Setting-up ..."
 cd /
 mkdir aerospike
 cd /aerospike
+clear
 
 echo "Install Aerospike ..."
 
-sudo apt-get update
+sudo apt-get -y update
 sudo wget -O aerospike-server.tgz http://www.aerospike.com/download/server/latest/artifact/debian7
 sudo tar -zxvf aerosopike-server.tgz
 cd aerospike-server-community-*
 sudo ./asinstall
+clear
 
 echo "Install Aerospike Management Console (AMC) ..."
 sudo wget -O aerospike-management-console.deb http://www.aerospike.com/download/amc/latest/artifact/debian6
 sudo dpkg -i aerospike-management-console.deb
-sudo apt-get -f install
+sudo apt-get -y -f install
+clear
 
 echo "Starting Services ..."
 sudo service aerospike start
